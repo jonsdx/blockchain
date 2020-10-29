9e524e0408462546f3fab5a52828ec457bd332bc
Shiguang Xu <sxu014@e.ntu.edu.sg>
2020-10-30 02:50:04 +0800

    v0.5
    adding front end template

---
+++ auction-page/src/DutchAuction.js
@@ -0,0 +1,7 @@
+import { useState, useRef, useEffect } from "react";
+import detectEthereumProvider from "@metamask/detect-provider";
+// NOTE: be aware of this: https://flaviocopes.com/parcel-regeneratorruntime-not-defined/
+import Web3 from "web3";
+
+// importing a compiled contract artifact which contains function signature etc. to interact
+import artifact from "../build/contracts/DutchAuction.json";
