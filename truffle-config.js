d2d16398fe24c04c1662992965de33c2675ae592
zy <teezhengyang16@gmail.com>
2020-10-31 15:49:04 +0800

    first

--- truffle-config.js
+++ truffle-config.js
@@ -43,15 +43,14 @@ module.exports = {
     // options below to some value.
     //
     // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
+    //   host: "127.0.0.1",     // Localhost (default: none)
+    //   port: 8545,            // Standard Ethereum port (default: none)
+    //   network_id: "*",       // Any network (default: none)
     // },
     test: {
       host: "127.0.0.1",
       port: 7545,
       network_id: 5777,
-      websockets: true
     },
     // Another network with more advanced options...
     // advanced: {
@@ -65,12 +64,12 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-    provider: () => new HDWalletProvider(mnemonic, `wss://ropsten.infura.io/ws/v3/f310feadf7c04024996d4c13a1fd0fbc`),
-    network_id: 3,       // Ropsten's id
-    gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      provider: () => new HDWalletProvider(mnemonic, `wss://ropsten.infura.io/ws/v3/f310feadf7c04024996d4c13a1fd0fbc`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
     // Useful for private networks
     // private: {
